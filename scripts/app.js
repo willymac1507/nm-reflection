a3e3915edc21055d6d0a5520ffba7aa0e623096e
Will McCloy <william.mccloy@googlemail.com>
2022-03-02 16:16:20 +0000

    Add cookies consent modal

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,23 @@ $(document).ready(() => {
         arrows: false,
         dots: true,
         pauseOnHover: false,
+        pauseOnFocus: false,
         pauseOnDotsHover: true,
-      });
+    });
+  const cookieConsent = sessionStorage.getItem('cookies');
+  console.log(cookieConsent);
+  if (!cookieConsent) {
+    $('#cookies').addClass('show');
+    $('body').css({height: '100vh', overflow: 'hidden'});
+  }
+});
+
+$('#supportButton').click(() => {
+  sessionStorage.removeItem('cookies');
+});
+
+$('.button--cookieAccept').click(() => {
+  sessionStorage.setItem('cookies', true);
+  $('#cookies').removeClass('show');
+  $('body').removeAttr("style");
 });
