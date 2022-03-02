3fc318ee84618bb57df1f726caa7b06916bea885
Will McCloy <william.mccloy@googlemail.com>
2022-03-02 10:35:13 +0000

    Add carousel functionality and buttons

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('.carousel__main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnDotsHover: true,
+      });
+});
