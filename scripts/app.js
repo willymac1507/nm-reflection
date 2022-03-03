9c3a8573ca7ee7cffc4c9b85341e2d6abebe3b5a
Will McCloy <william.mccloy@googlemail.com>
2022-03-03 12:53:07 +0000

    Complete sticky navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,28 @@
-$(document).ready(() => {
-    $('.carousel__main').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        arrows: false,
-        dots: true,
-        pauseOnHover: false,
-        pauseOnFocus: false,
-        pauseOnDotsHover: true,
-    });
+$(function () {
+  $("header").clone().insertBefore('header').addClass("header--hidden");
+  $('.carousel__main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    pauseOnDotsHover: true,
+  });
   const cookieConsent = sessionStorage.getItem('cookies');
   console.log(cookieConsent);
   if (!cookieConsent) {
     $('#cookies').addClass('show');
-    $('body').css({height: '100vh', overflow: 'hidden'});
-  }
+    $('body').css({
+      height: '100vh',
+      overflow: 'hidden'
+    });
+  };
 });
+
+let pagePosition = 0;
 
 $('#supportButton').click(() => {
   sessionStorage.removeItem('cookies');
@@ -26,4 +32,32 @@ $('.button--cookieAccept').click(() => {
   sessionStorage.setItem('cookies', true);
   $('#cookies').removeClass('show');
   $('body').removeAttr("style");
+});
+
+$(document).on('scroll', () => {
+
+  const headHeight = $('header').height();
+  let scrollValue = $(document).scrollTop();
+
+  if (scrollValue < pagePosition)
+  // User is scrolling up
+  {
+    if (scrollValue > headHeight) {
+      console.log('hidden');
+      $('.header--hidden').addClass('header--animated').addClass('header--sticky');
+    } else if (scrollValue === 0) {
+      console.log('top');
+      $('.header--hidden').removeClass('header--sticky');
+    } else {
+      console.log('visible');
+      $('.header--hidden').removeClass('header--animated');
+    }
+  } else if (scrollValue > pagePosition)
+  // User is scrolling down
+  {
+    $('.header--hidden').removeClass('header--sticky');
+  } else {
+    console.log('top');
+  };
+  pagePosition = scrollValue;
 });
