3be5059d4d82c6de090c880bd6272becea3de232
Will McCloy <william.mccloy@googlemail.com>
2022-03-30 16:46:27 +0100

    Fix console errors and scrollbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 $(function () {
   // Create clone of header and hide above body
   const headHeight = $('header').height();
-  $("header").clone().insertBefore('header').addClass("header--hidden");
+  $("header").clone().insertBefore('header').addClass("header--hidden").removeClass('header--fixed').css('width', `${headerWidth()}px`);
 
   // Instantiate slick carousel plugin
   $('.carousel__main').slick({
@@ -15,8 +15,6 @@ $(function () {
     pauseOnFocus: false,
     pauseOnDotsHover: true,
   });
-
-  // Instantiate scrollPane plugin
 
   // Check if cookie consent has been given in this session
   const cookieConsent = sessionStorage.getItem('cookies');
@@ -50,19 +48,24 @@ function sbWidth() {
   return sbWid;
 }
 
+function headerWidth() {
+  const head = $('.header--fixed').width();
+  return head;
+}
+
 let pagePosition = 0;
 
 // Handle scroll event to determine whether to show sticky navbar
-$(document).on('scroll', (e) => {
+$('.main__holder').on('scroll', (e) => {
 
-  let scrollValue = $(document).scrollTop();
+  let scrollValue = $('.main__holder').scrollTop();
   const headHeight = $('header').height();
 
   if (scrollValue < pagePosition)
   // User is scrolling up
   {
     if (scrollValue > headHeight) {
-      $('.header--hidden').addClass('header--animated').addClass('header--sticky');
+      $('.header--hidden').addClass('header--animated').addClass('header--sticky').css('width', `${headerWidth()}px`);
     } else if (scrollValue === 0) {
       $('.header--hidden').removeClass('header--sticky');
     } else {
@@ -85,14 +88,14 @@ $('.page__container').on('click', (e) => {
   if (clicked.classList.contains('button--ham')) {
     $('.page__container').css('left', sbWidth).addClass('sidebar__shown');
     $('.header--hidden').css('left', sbWidth);
-    $('body').css('overflow-y', 'hidden');
+    // $('body').css('top', -(document.documentElement.scrollTop) + 'px').addClass('noScroll');
     $('.hamburger').addClass('hamburger--open');
 
   } else if (clicked.classList.contains('sidebar__overlay')) {
     $('.page__container').removeClass('sidebar__shown').removeAttr('style');
-    $('body').removeAttr('style');
+    // $('body').removeAttr('style').removeClass('noScroll');
     $('.sidebar__container').scrollTop(0);
-    $('.header--hidden').removeAttr('style');
+    $('.header--hidden').removeAttr('style').css('width', `${headerWidth()}px`);
     $('.hamburger').removeClass('hamburger--open');
 
   } else {
