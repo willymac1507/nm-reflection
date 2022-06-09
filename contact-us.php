84a1b1bc194ada0110fabf87004da6f7c8364085
Will McCloy <william.mccloy@googlemail.com>
2022-06-09 12:59:07 +0100

    Add content to office cards

--- contact-us.php
+++ contact-us.php
@@ -1,6 +1,7 @@
 <?php
 include 'inc/header.php';
 echo '<main>';
+include 'inc/contact.php';
 include 'inc/newsletter.php';
 echo '</main>';
 include 'inc/footer.php';
--- inc/contact.php
+++ inc/contact.php
@@ -1,7 +1,70 @@
+<?php
+// code here
+?>
 <section id="contact">
-<div class="card card--contact">
-  <img src="" alt="" class="office__image">
-</div>
+  <div class="contact">
+    <div class="contact__header">
+      <h1 class="contact__headline">Our Offices</h1>
+    </div>
+    <div class="contact__content">
+
+      <div class="card card--contact">
+        <img src="/images/cambridge.jpg" alt="Cambridge Office" class="office__image">
+        <div class="contact__details">
+          <a href="#" class="office__name">Cambridge Office</a>
+          <div class="office__address">
+            Unit 1.28,<br>
+            St John's Innovation Centre,<br>
+            Cowley Road, Milton<br>
+            Cambridge,<br>
+            CB4 0WS
+          </div>
+          <a href="#" class="office__phone">01223 37 57 72</a>
+          <button class="button button--office" >view more</button >
+        </div>
+      </div>
+
+      <div class="contact__map"></div>
+
+      <div class="card card--contact">
+        <img src="/images/wymondham.jpg" alt="Wymondham Office" class="office__image">
+        <div class="contact__details">
+          <a href="#" class="office__name">Wymondham Office</a>
+          <div class="office__address">
+            Unit 15,<br>
+            Penfold Drive,<br>
+            Gateway 11 Business Park,<br>
+            Wymondham, Norfolk,<br>
+            NR18 0WZ
+          </div>
+          <a href="#" class="office__phone">01603 70 40 20</a>
+          <button class="button button--office" >view more</button >
+        </div>
+      </div>
+
+      <div class="contact__map"></div>
+
+      <div class="card card--contact">
+        <img src="/images/yarmouth-2.jpg" alt="Yarmouth Office" class="office__image">
+        <div class="contact__details">
+          <a href="#" class="office__name">Great Yarmouth Office</a>
+          <div class="office__address">
+            Suite F23,<br>
+            Beacon Innovation Centre,<br>
+            Beacon Park, Gorleston<br>
+            Great Yarmouth, Norfolk,<br>
+            NR31 7RA
+          </div>
+          <a href="#" class="office__phone">01493 60 32 042</a>
+          <button class="button button--office" >view more</button >
+        </div>
+      </div>
+
+      <div class="contact__map"></div>
+
+    </div>
+
+  </div>
 </section>
 
 
