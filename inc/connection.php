04b406e13c4bdea08282c0018c19513e9f47bb30
Will McCloy <william.mccloy@googlemail.com>
2022-06-28 14:05:41 +0100

    Initial commit

--- inc/connection.php
+++ inc/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 try {
-//    $db = new PDO("mysql:host=localhost:8889;dbname=netmatters_reflection;", "root", "root");
-    $db = new PDO("mysql:host=localhost:3306;dbname=willmccl_netmatters_reflection;", "willmccl_admin", 'MyDbAdmin01!');
+    $db = new PDO("mysql:host=localhost:8889;dbname=netmatters_reflection;", "root", "root");
+//    $db = new PDO("mysql:host=localhost:3306;dbname=willmccl_netmatters_reflection;", "willmccl_admin", 'MyDbAdmin01!');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     echo "Unable to connect to the database.";
